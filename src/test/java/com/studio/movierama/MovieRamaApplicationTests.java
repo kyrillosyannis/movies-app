91ca3ca275318edb227a409bda2f4ce7d739d6e9
Yannis Kyrillos <yannis.kyrillos@prodege.com>
2022-08-31 12:08:36 +0300

    initial commit

---
+++ src/test/java/com/studio/movierama/MovieRamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studio.movierama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
